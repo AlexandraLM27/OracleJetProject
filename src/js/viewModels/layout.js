6cacbbe74fd8f3033b796b7a644c3f5d5ebb94fe
AlexandraLM27 <luminitaalexandra.mesina@ulbsibiu.ro>
2025-01-11 20:01:42 +0200

    Laborator 4

--- src/js/viewModels/layout.js
+++ src/js/viewModels/layout.js
@@ -8,12 +8,91 @@
 /*
  * Your incidents ViewModel code goes here
  */
-define(['../accUtils'],
- function(accUtils) {
+
+define(['../accUtils', "knockout", "ojs/ojarraydataprovider", 
+"ojs/ojresponsiveutils", "ojs/ojresponsiveknockoututils", "text!../basicData.json",  "text!../CatalogData.json", "ojs/ojtable", "ojs/ojselectcombobox","ojs/ojchart", "ojs/ojcheckboxset", "ojs/ojnavigationlist", "ojs/ojknockout",  "ojs/ojcollapsible"],
+ function(accUtils, ko, ArrayDataProvider, ResponsiveUtils, ResponsiveKnockoutUtils, dataChart, dataCatalog) {
     function LayoutViewModel() {
       // Below are a set of the ViewModel methods invoked by the oj-module component.
       // Please reference the oj-module jsDoc for additional information.
-
+      this.checkValue = ko.observableArray();
+      this.dircolumn = ko.pureComputed(() => {
+          return !!(typeof this.checkValue()[0] !== 'undefined' &&
+              this.checkValue()[0] != null &&
+              this.checkValue()[0] === 'dirColumn');
+      });
+      const data = [ 
+        { name: "Grafic Bar", id: "bar", icons: "oj-ux-ico-bar-chart" }, 
+        { name: "Grafic Line", id: "line", icons: "oj-ux-ico-share" }, 
+        { name: "Grafic Pie", id: "pie", icons: "oj-ux-ico-success-s" }, 
+        { name: "Grafic Area", id: "area", icons: "oj-ux-ico-chat-on" } 
+        ]; 
+        this.dataProvider = new ArrayDataProvider(data, { keyAttributes: "id" }); 
+        // observable for medium and up screens 
+        this.selectedItem = ko.observable("bar"); 
+        let mdQuery = ResponsiveUtils.getFrameworkQuery("md-up"); 
+        if (mdQuery) { 
+        this.medium = ResponsiveKnockoutUtils.createMediaQueryObservable(mdQuery); 
+        }
+       /* toggle button variables */ 
+       this.stackValue = ko.observable('off'); 
+       this.orientationValue = ko.observable('vertical'); 
+       this.dataProviderChart = new ArrayDataProvider(JSON.parse(dataChart), { 
+           keyAttributes: 'id' 
+       }); 
+       this.columns = [{ 
+        headerText: "Id", 
+        field: "id", 
+        headerClassName: "oj-sm-only-hide", 
+        className: "oj-sm-only-hide", 
+        resizable: "enabled", 
+        id: "id" 
+      }, 
+      { 
+        headerText: "Nume Prenume", 
+        template: "fullName",       
+        headerClassName: "oj-sm-only-hide", 
+        className: "oj-sm-only-hide", 
+        resizable: "enabled", 
+        id: "fullname" 
+        }, 
+      { 
+        headerText: "Algoritmi", 
+        field: "alg", 
+        headerClassName: "oj-sm-only-hide", 
+        className: "oj-sm-only-hide", 
+        resizable: "enabled", 
+        id: "alg" 
+      }, 
+      { 
+        headerText: "ASC", 
+        field: "asc", 
+        headerClassName: "oj-sm-only-hide", 
+        className: "oj-sm-only-hide", 
+        resizable: "enabled", 
+        id: "asc" 
+      }, 
+      { 
+        headerText: "Birotica", 
+        field: "bir", 
+        headerClassName: "oj-sm-only-hide", 
+        className: "oj-sm-only-hide", 
+        resizable: "enabled", 
+        id: "bir" 
+      },
+      { 
+        headerText: "Media", 
+        template: "media",       
+        headerClassName: "oj-sm-only-hide", 
+        className: "oj-sm-only-hide", 
+        resizable: "enabled", 
+        id: "media" 
+        }]; 
+      this.CatalogArray = JSON.parse(dataCatalog); 
+      this.dataproviderCatalog = new ArrayDataProvider(this.CatalogArray, { 
+        keyAttributes: 'id', 
+        implicitSort: [{ attribute: 'id', direction: 'ascending' }] 
+      }); 
       /**
        * Optional ViewModel method invoked after the View is inserted into the
        * document DOM.  The application can put logic that requires the DOM being
