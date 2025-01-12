74e262f0407d2632d78927bdcc967fc5d56e29cf
AlexandraLM27 <luminitaalexandra.mesina@ulbsibiu.ro>
2025-01-12 17:33:18 +0200

    Laaboratorul 6

--- src/js/viewModels/forms.js
+++ src/js/viewModels/forms.js
@@ -8,48 +8,80 @@
 /*
  * Your customer ViewModel code goes here
  */
-define(['../accUtils'],
-    function(accUtils) {
-       function FormsiewModel() {
-         // Below are a set of the ViewModel methods invoked by the oj-module component.
-         // Please reference the oj-module jsDoc for additional information.
-   
-         /**
-          * Optional ViewModel method invoked after the View is inserted into the
-          * document DOM.  The application can put logic that requires the DOM being
-          * attached here.
-          * This method might be called multiple times - after the View is created
-          * and inserted into the DOM and after the View is reconnected
-          * after being disconnected.
-          */
-         this.connected = () => {
-           accUtils.announce('Forms page loaded.', 'assertive');
-           document.title = "Forms";
-           // Implement further logic if needed
-         };
-   
-         /**
-          * Optional ViewModel method invoked after the View is disconnected from the DOM.
-          */
-         this.disconnected = () => {
-           // Implement if needed
-         };
-   
-         /**
-          * Optional ViewModel method invoked after transition to the new View is complete.
-          * That includes any possible animation between the old and the new View.
-          */
-         this.transitionCompleted = () => {
-           // Implement if needed
-         };
-       }
-   
+define(['../accUtils', "knockout", 'text!../basicData.json', 'ojs/ojarraydataprovider', "ojs/ojknockout", 
+  "ojs/ojbutton", "ojs/ojinputtext", "ojs/ojformlayout", "ojs/ojinputnumber", "ojs/ojradioset", "ojs/ojlabel", "ojs/ojlabelvalue", 'ojs/ojchart', "ojs/ojcheckboxset", "ojs/ojlabel", "ojs/ojlabelvalue" ],
+    function(accUtils, ko, data, ArrayDataProvider) {
+       function FormsViewModel() {
+        this.value_disabled = ko.observable(false);
+        this.value_readonly = ko.observable(false);
+        this.activatedButton = ko.observable("(None activated yet)");
+        
+        this.buttonAction = (event) => { 
+          this.activatedButton(event.currentTarget.id); 
+          if (String(event.currentTarget.id) === "disabled") { 
+          this.value_disabled(true); 
+          this.value_readonly(false); 
+          } else 
+          if (String(event.currentTarget.id) === "readonly") { 
+          this.value_disabled(false); 
+          this.value_readonly(true); 
+          } else { 
+          this.value_disabled(false); 
+          this.value_readonly(false); 
+          } 
+          return true; 
+          };
+        this.nume = ko.observable("Andreea");
+        this.prenume = ko.observable("Popescu");
+        this.nume_prenume = ko.computed(function() { 
+          return this.nume() + " " + this.prenume(); 
+        }.bind(this)); 
+        this.varsta = ko.observable(20)
+        this.setModelCurrentChartToInit = () => { 
+          this.currentChart("bar"); 
+          return true;      
+  }; 
+         
+        this.currentChart = ko.observable("bar"); 
+        this.chartOptions = [ { id: "area", value: "Area" }, 
+          { id: "bar", value: "Bar" }, 
+          { id: "line", value: "Line" }, 
+          { id: "pie", value: "Pie" }, 
+          { id: "funnel", value: "Funnel" } ]; 
+        self.stackValue = ko.observable('off');
+        self.orientationValue = ko.observable('vertical');
+        self.dataProvider = new ArrayDataProvider(JSON.parse(data), {
+          keyAttributes: 'id'});
+        this.orientationValue = ko.observable("vertical"); 
+        this.directionOptions = [ 
+            { id: "vertical", value: "Vertical" }, 
+            { id: "horizontal", value: "Horizontal" } 
+          ]; 
+          this.currentJudet = ko.observableArray(['sibiu']);
+          this.agreement = ko.observableArray();
+          this.judetOptions = [
+            { id: 'sb', value: 'sibiu', judet: 'Sibiu' },
+            { id: 'cj', value: 'cluj', judet: 'Cluj' },
+            { id: 'vl', value: 'valcea', judet: 'Valcea' },
+            { id: 'bv', value: 'brasov', judet: 'Brasov' },
+            { id: 'ab', value: 'alba', judet: 'Alba' }
+          ];
+          this.setJudeteSibiuValcea = () => { 
+            this.currentJudet(['sibiu', 'valcea']); 
+            return true;      
+    }; 
+    
+          this.setJudetToSibiu = () => { 
+            this.currentJudet(['sibiu']); 
+            return true;      
+      }; 
+  }
        /*
         * Returns an instance of the ViewModel providing one instance of the ViewModel. If needed,
         * return a constructor for the ViewModel so that the ViewModel is constructed
         * each time the view is displayed.
         */
-       return FormsiewModel;
+       return FormsViewModel;
      }
    );
    
